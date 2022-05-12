d64f27f553d3e7ae6c78ab534ba4b6c934e29f12
Анатолий Жмышенко <gggame184@gmail.com>
2022-05-12 10:53:57 +0300

    Created a database and added logic to it

---
+++ CameraXamarin/CameraXamarin/db/CRUDOoperation.cs
@@ -0,0 +1,36 @@
+﻿using CameraXamarin.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CameraXamarin.db
+{
+    public class CRUDOoperation
+    {
+        SQLiteConnection DB;
+        public CRUDOoperation(string database)
+        {
+            DB = new SQLiteConnection(database);
+            DB.CreateTable<ProjectPhoto>();
+        }
+
+        public IEnumerable<ProjectPhoto> GetProjectPhotos()
+        {
+            return DB.Table<ProjectPhoto>();
+        }
+        public int SaveItem(ProjectPhoto projectphoto)
+        {
+            if (projectphoto.Id != 0)
+            {
+                DB.Update(projectphoto);
+                return projectphoto.Id;
+            }
+
+            else
+            {
+                return DB.Insert(projectphoto);
+            }
+        }
+    }
+}
---
+++ CameraXamarin/CameraXamarin/db/ISqlite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CameraXamarin.db
+{
+    public interface ISqlite
+    {
+        string GetDatabase(string filename);
+    }
+}
